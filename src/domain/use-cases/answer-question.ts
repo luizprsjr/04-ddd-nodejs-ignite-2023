e7c5eeabeb6dc8882c67bc1b5a83e24c3132920e
Luiz Pires <lpiresjunior@gmail.com>
2023-06-10 02:03:56 -0300

    feat: abstract entity creation with static 'create' method

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
 import { Answer } from '../entities/answer'
 import { AnswersRepository } from '../repositories/answers-repositories'
 
@@ -12,10 +13,10 @@ export class AnswerQuestion {
 
 
   async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
+    const answer = Answer.create({
       content,
-      autorId: instructorId,
-      questionId
+      autorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId)
     })
 
     await this.answersRepository.create(answer)
