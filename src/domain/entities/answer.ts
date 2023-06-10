6df9311c329ba40bd680932ca4176a72edb96577
Luiz Pires <lpiresjunior@gmail.com>
2023-06-10 01:49:23 -0300

    feat: perform property mapping for Answer and Question entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from '../../core/entities/entity'
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface AnswerProps {
+  autorId: UniqueEntityID
+  questionId: UniqueEntityID
   content: string
-  autorId: string
-  questionId: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
