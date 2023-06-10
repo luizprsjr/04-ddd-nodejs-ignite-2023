203190c654477d7e67e4e1bd12f52f3b14b515bd
Luiz Pires <lpiresjunior@gmail.com>
2023-06-10 01:34:46 -0300

    feat: implement base Entity class

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from 'crypto'
+import { Entity } from '../../core/entities/entity'
 
 interface AnswerProps {
   content: string
@@ -6,16 +6,8 @@ interface AnswerProps {
   questionId: string
 }
 
-export class Answer {
-  public id: string
-  public content: string
-  public autorId: string
-  public questionId: string
-
-  constructor(props: AnswerProps, id?: string) {
-    this.content = props.content
-    this.autorId = props.autorId
-    this.questionId  = props.questionId 
-    this.id = id ?? randomUUID()
+export class Answer extends Entity<AnswerProps> {
+  get content() {
+    return this.props.content
   }
 }
